40f108d29db12c0f20d09aca96eed256d96c0900
David Markey <david@dmarkey.com>
2015-06-24 17:21:41 +0100

    iteration

--- app/application.cpp
+++ app/application.cpp
@@ -38,175 +38,6 @@ HttpServer server;
 bool httpInProgress = false;
 
 
-struct QueuedRequest{
-    String url;
-    String post_data;
-    String content_type;
-};
-
-template <class T>
-class QueueNode
-{
-public:
-	T data;
-	QueueNode *next;
-	QueueNode(const T info, QueueNode *nextValue = NULL)
-	{
-		data = info;
-		next = nextValue;
-	}
-	QueueNode(QueueNode *nextValue = NULL)
-	{
-		next = nextValue;
-	}
-};
-
-// Queue class.
-template <class T>
-class Queue
-{
-private:
-	QueueNode<T> *front;
-	QueueNode<T> *rear;
-	int size;
-
-public:
-	Queue();
-	~Queue();
-
-	// Check if queue is empty.
-	bool IsEmpty();
-
-	// Clear the queue.
-	void Clear();
-
-	// Add an item to the end of the queue.
-	bool EnQueue(T item);
-
-	// Get an item from the front of the queue.
-	T DeQueue();
-
-	// Get an item from the front of the queue without removing it.
-	T GetFirstNode();
-
-	// Print contents of the queue.
-	void Print();
-};
-
-template <class T>
-Queue<T>::Queue()
-{
-	// Create empty queue with front and rear pointing to NULL.
-	front = rear = NULL;
-}
-
-template <class T>
-Queue<T>::~Queue()
-{
-	Clear();
-}
-
-template <class T>
-bool Queue<T>::IsEmpty()
-{
-	// If front is NULL then the queue is empty.
-	return front == NULL;
-}
-
-template <class T>
-void Queue<T>::Clear()
-{
-	QueueNode<T> *tmp;
-
-	// Go through each node until the end of the queue.
-	while (front != NULL)
-	{
-		// Point tmp to next node.
-		tmp = front->next;
-
-		// Delete current node.
-		delete front;
-
-		// Point front to next node.
-		front = tmp;
-	}
-}
-
-template <class T>
-bool Queue<T>::EnQueue(T value)
-{
-	// Create new node.
-	QueueNode<T> *node = new QueueNode<T>(value);
-	size++;
-
-	// If queue is empty then point both front and rear to the new node.
-	if (IsEmpty())
-	{
-		front = rear = node;
-		return true;
-	}
-
-	// Add node to the end of the queue and repoint rear to the new node.
-	rear->next = node;
-	rear = node;
-
-	return true;
-}
-
-template <class T>
-T Queue<T>::DeQueue()
-{
-	// If queue is empty return NULL.
-	if (IsEmpty())
-	{
-		return T();
-	}
-
-	// Save value from top node.
-	T value = front->data;
-
-	// Point tmp to front.
-	QueueNode<T> *tmp = front;
-
-	// Repoint front to the second node in the queue.
-	front = front->next;
-
-	// Remove first node.
-	delete tmp;
-
-	// Update queue size.
-	size--;
-
-	return value;
-}
-
-template <class T>
-T Queue<T>::GetFirstNode()
-{
-	// If queue is empty return NULL.
-	if (IsEmpty())
-	{
-		return T();
-	}
-
-	return front->data;
-}
-
-template <class T>
-void Queue<T>::Print()
-{
-	QueueNode<T> *tmp = front;
-	while (tmp != NULL)
-	{
-		//cout << tmp->data << " ";
-		tmp = tmp->next;
-	}
-}
-
-
-Queue<QueuedRequest> web_queue;
-
-
 String mqttName(){
     String name;
     int id = system_get_chip_id();
@@ -247,37 +78,9 @@ bool processing_web = false;
 
 void printResponse(HttpClient& hc, bool success);
 
-void process_web(){
-    if (processing_web == true){
-        return;
-    }
-    if (web_queue.IsEmpty()){
-        processing_web = false;
-        return;
-    }
-    QueuedRequest request = web_queue.DeQueue();
-
-    hc.setRequestContentType(request.content_type);
-    hc.setPostBody(request.post_data);
-    hc.downloadString(request.url, printResponse);
-
-}
-
-
-void queue_web_request(String url, String post_data, String content_type="multipart/form-data"){
-    QueuedRequest qr;
-    qr.post_data = post_data;
-    qr.url = url;
-    qr.content_type = content_type;
-    web_queue.EnQueue(qr);
-    process_web();
-
-}
-
 void printResponse(HttpClient& hc, bool success){
     Serial.print(hc.getResponseString());
     processing_web = false;
-    process_web();
 }
 
 void ICACHE_FLASH_ATTR push_to_register()
@@ -305,42 +108,26 @@ void ICACHE_FLASH_ATTR set_switch(int swit, bool state)
 }
 
 
-
-
-
-// Publish our message
-void publishMessage()
-{
-	Serial.println("Let's publish message now!");
-	mqtt.publish("main/frameworks/sming", "Hello friends, from Internet of things :)"); // or publishWithQoS
-}
-
 void processSwitchcmd(JsonObject& obj){
     int switch_num = obj["socketnumber"];
     bool state = obj["state"];
     set_switch(switch_num, state);
 
-    //ack_task()
 }
 
 
-
-
-void ackTask(JsonObject& obj){
+void setTaskStatus(JsonObject& obj, int status){
 
     char post_data[256];
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
     root["controller_id"] = system_get_chip_id();
     root["task_id"] = obj["task_id"];
-    root["status"] = 2;
+    root["status"] = status;
 
     root.printTo(post_data, sizeof(post_data));
-    queue_web_request("http://dmarkey.com:8080/controller_task_status/", post_data, "application/json");
-
-
-
-
+    mqtt.publish("/admin/task_status", post_data);
+    //queue_web_request("http://dmarkey.com:8080/controller_task_status/", post_data, "application/json");
 }
 
 
@@ -355,12 +142,14 @@ void onMessageReceived(String topic, String message)
         JsonObject& root = jsonBuffer.parseObject((char *)json);
 
         const char * command = root["name"];
+        setTaskStatus(root, 2);
+
         if (strcmp(command, "sockettoggle") != -1){
                 processSwitchcmd(root);
 
         }
 
-        ackTask(root);
+        setTaskStatus(root, 3);
 	}
 
 }
@@ -368,16 +157,30 @@ void onMessageReceived(String topic, String message)
 
 
 
+void processBeaconResponse(HttpClient& hc, bool success){
+    Serial.print(hc.getResponseString());
+    if (success){
+        mqtt.connect(mqttName());
+        mqtt.subscribe(commandTopic());
+    }
+
+    //processing_web = false;
+    //process_web();
+}
+
+
+
+
+
 
 void beaconFunc(){
-    HttpClient hc;
     String post_data;
     post_data = "model=Smarthome2&controller_id=";
     post_data += system_get_chip_id();
     post_data += "\r\n";
-    queue_web_request("http://dmarkey.com:8080/controller_ping_create/", post_data);
-    /*hc.setPostBody(post_data);
-    hc.downloadString(", printResponse);*/
+    //queue_web_request("http://dmarkey.com:8080/controller_ping_create/", post_data);
+    hc.setPostBody(post_data);
+    hc.downloadString("http://dmarkey.com:8080/controller_ping_create/", processBeaconResponse);
 }
 
 
@@ -390,8 +193,7 @@ void connectOk()
 	Serial.println("I'm CONNECTED");
     beaconFunc();
 	// Run MQTT client
-	mqtt.connect(mqttName());
-	mqtt.subscribe(commandTopic());
+
 }
 
 void writeConf(String SSID, String Pwd){
